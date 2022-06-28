d7497957cffadcc562f4594e04e066e0fa4fcc9f
Zulkariski Mauladi <74296102+zulkamaula@users.noreply.github.com>
2022-06-28 16:14:39 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Jumbotron, Container } from 'reactstrap';
 import './App.css';
 import NavbarComponent from './components/NavbarComponent'
 
-function App() {
-  return (
-    <div>
-      <NavbarComponent />
-      
-      <Jumbotron fluid>
-        <Container fluid>
-          <main>
-            Hello world
-          </main>
-        </Container>
-      </Jumbotron>
-    </div>
-    
-  );
+export default class App extends Component {
+  render() {
+    return (
+       <div>
+        <NavbarComponent />
+        
+        <Jumbotron fluid>
+          <Container fluid>
+            <main>
+              Hello world
+            </main>
+          </Container>
+        </Jumbotron>
+      </div>
+    )
+  }
 }
-
-export default App;
